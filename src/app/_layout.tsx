a84bb86c5378d929b10462d74b3437d6e485a055
jatinscodingproject <jatincoder104@gmail.com>
2025-09-26 15:04:35 +0530

    Setup Expo Router

---
+++ src/app/_layout.tsx
@@ -0,0 +1,20 @@
+// app/_layout.tsx
+import { Stack } from 'expo-router';
+import { DarkTheme, ThemeProvider } from '@react-navigation/native';
+
+export default function RootLayout() {
+  return (
+    <ThemeProvider value={DarkTheme}>
+      <Stack>
+        <Stack.Screen
+          name="index"
+          options={{
+            title: 'Event',
+            headerLargeTitle: true,
+          }}
+        />
+      </Stack>
+    </ThemeProvider>
+  );
+}
+ 
